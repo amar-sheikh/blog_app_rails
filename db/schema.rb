74e0989020ceac210e922aa5bea1ebd747b832e2
umar <umar.m.sheikh@gmail.com>
2025-06-17 15:03:47 +0500

    Add missing column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_13_160409) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_17_100247) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_13_160409) do
     t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
