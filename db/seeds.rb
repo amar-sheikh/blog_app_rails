46b7c4f9569283efc933dd21449025e450661c72
umar <umar.m.sheikh@gmail.com>
2025-06-18 18:38:59 +0500

    Seeding data into data base

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,61 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+
+require 'faker'
+
+Comment.delete_all
+Article.delete_all
+Tag.delete_all
+User.delete_all
+
+users = 25.times.map do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    type: nil
+  )
+end
+
+authors = 25.times.map do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    type: 'Author'
+  )
+end
+
+tags = 20.times.map do
+  Tag.create!(
+    name: Faker::Lorem.unique.word,
+    description: Faker::Lorem.sentence
+  )
+end
+
+articles = 200.times.map do
+  published  = [true, false].sample
+
+  Article.create!(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 5).join("\n"),
+    published: published,
+    author_id: authors.sample.id,
+    published_at: published ? Time.at(rand(30.days.ago.to_f..2.hours.ago.to_f)) : nil
+  )
+end
+
+articles.each do |article|
+  article.tags << tags.sample(rand(1..4))
+end
+
+comments = 500.times.map do
+  Comment.create!(
+    content: Faker::Lorem.sentence,
+    user_id: users.sample.id,
+    article_id: articles.sample.id
+  )
+end
